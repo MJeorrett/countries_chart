d1f3a828d07dd659daa7bd7419afbbc2dcbd2351
Carlos Pereira <carlinhop@gmail.com>
2016-12-08 12:04:14 +0000

    Parsed data into array and added chart div

--- public/app.js
+++ public/app.js
@@ -1,5 +1,15 @@
 window.onload = function() {
   var countries = xmlhttpHelper.getRequest( "https://restcountries.eu/rest/v1/all", function( countries ) {
     console.log( "received countries", countries );
+
+
+    var countryArray = countries.map(function(country){
+      return {name: country.name, population: country.population, code: country.alpha2Code};
+    });
+
+    console.log(countryArray);
+
+
+
   });
 };
